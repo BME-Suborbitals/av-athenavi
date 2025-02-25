4aa5582103aa215060bbcf58598e02cf5bdaf14f
Török Péter <tpeti0730@gmail.com>
2025-02-25 11:54:37 +0100

    Added BME280 driver usage code

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -1,15 +1,40 @@
 #include "gpio.h"
+#include "bme280.h"
+#include "i2c.h"
+#include "i2c_device.h"
 
 extern "C" void SystemClock_Config(void);
 
 int main() {
     HAL_Init();
     SystemClock_Config();
-
     MX_GPIO_Init();
+    MX_I2C1_Init();
+
+    /*communication::I2CDevice i2c_mmc(hi2c1, 0x30);
+    sensor::MMC5983MA mmc(&i2c_mmc);
+    mmc.Initialize(sensor::MMC5983MA::BANDWIDTH_100_HZ);*/
+
+    communication::I2CDevice i2c_bme(hi2c1, 0x76);
+    sensor::BME280 bme(&i2c_bme);
+    if(!bme.Initialize(sensor::BME280::Oversampling::SAMPLE_STANDARD,
+                       sensor::BME280::Oversampling::SAMPLE_STANDARD,
+                       sensor::BME280::Oversampling::SAMPLE_STANDARD,
+                       sensor::BME280::PowerMode::MODE_NORMAL,
+                       sensor::BME280::StandbyTime::STANDBY_62_5_MS,
+                       sensor::BME280::Filter::FILTER_2)){
+        while(1);
+    }
+    
 
     while (true) {
-        HAL_GPIO_TogglePin(LED_GPIO_Port, LED_Pin);
+        /*sensor::MMC5983MA::Data mmc_data;
+        mmc.Read(mmc_data);*/
+
+        sensor::BME280::Data bmp_data;
+        bme.Read(bmp_data);
+
+        //HAL_GPIO_TogglePin(LED_GPIO_Port, LED_Pin);
         HAL_Delay(1000);
     }
 }
