6cfcb361a31696faf8c79cee0b0a26858e81ed3d
Boldizsár Tóth <39193138+sn4k3ch4rm3r@users.noreply.github.com>
2025-03-01 00:47:17 +0100

    Configure hardware watchdog and added RTOS watchdog task

---
+++ app/src/tasks/watchdog_task.h
@@ -0,0 +1,27 @@
+#ifndef WATCHDOG_TASK_H
+#define WATCHDOG_TASK_H
+
+#include <chrono>
+#include <vector>
+#include "FreeRTOS.h"
+#include "FreeRTOSConfig.h"
+#include "monitored_task.h"
+#include "portmacro.h"
+#include "rtos_task.h"
+
+namespace tasks {
+class WatchdogTask : public rtos::Task {
+  private:
+    std::chrono::milliseconds task_timeout_;
+    std::vector<MonitoredTask*> monitored_tasks_;
+
+  protected:
+    void Run() override;
+
+  public:
+    WatchdogTask(std::chrono::milliseconds task_timeout, UBaseType_t priority = configMAX_PRIORITIES - 1, StackType_t stack_size = configMINIMAL_STACK_SIZE);
+    void RegisterTask(MonitoredTask* task);
+};
+}  // namespace tasks
+
+#endif  //WATCHDOG_TASK_H
