c6c81e7d58360761feb6bec58ca49643ff4a12a5
Boldizsár Tóth <39193138+sn4k3ch4rm3r@users.noreply.github.com>
2025-03-02 21:25:31 +0100

    Added doxygen comments to watchdog  and monitored  tasks

--- app/src/tasks/monitored_task.h
+++ app/src/tasks/monitored_task.h
@@ -11,11 +11,25 @@ class MonitoredTask : public rtos::Task {
     bool is_alive_{true};
 
   protected:
+    /**
+     * @brief Signals that the task is still alive. This must be called 
+     * periodically to ensure the watchdog doesn't reset the system.
+     */
     void Heartbeat();
 
   public:
     MonitoredTask(const char* name, StackType_t stack_size, UBaseType_t priority);
+
+    /**
+     * @brief Used by the watchdog to check if the task is running as expected
+     * @return true, if `Heartbeat` has been called since it has last expired.
+     */
     [[nodiscard]] bool IsAlive() const;
+
+    /**
+     * @brief Sets `IsAlive` to fasle. `Heartbeat` needs to bee called again
+     * before the next check.
+     */
     void ExpireHeartbeat();
 };
 }  // namespace tasks
