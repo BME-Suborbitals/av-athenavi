55797fa9997f4d4824ea57a059b3a71816c576eb
Boldizsár Tóth <39193138+sn4k3ch4rm3r@users.noreply.github.com>
2025-03-02 21:25:31 +0100

    Added doxygen comments to watchdog  and monitored  tasks

--- app/src/tasks/watchdog_task.h
+++ app/src/tasks/watchdog_task.h
@@ -20,6 +20,11 @@ class WatchdogTask : public rtos::Task {
 
   public:
     WatchdogTask(std::chrono::milliseconds task_timeout, UBaseType_t priority = configMAX_PRIORITIES - 1, StackType_t stack_size = configMINIMAL_STACK_SIZE);
+
+    /**
+     * @brief Adds a task to be monitored.
+     * @param task The task to monitor
+     */
     void RegisterTask(MonitoredTask* task);
 };
 }  // namespace tasks
